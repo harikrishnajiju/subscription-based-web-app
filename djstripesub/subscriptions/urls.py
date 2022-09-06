4338b40d377e6f61aa361fd3fc9b580654276aed
Harikrishna Jiju <harikrishnajijup@gmail.com>
2022-09-07 02:47:59 +0530

    first push

---
+++ djstripesub/subscriptions/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='subscriptions-home'),
+    path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
+    path('success/', views.success),  # new
+    path('cancel/', views.cancel),  # new
+    path('webhook/', views.stripe_webhook),
+]
